99c7c51574dc3a538cb9d6ab42cd94c0a67c66b4
Syd Sisco <hello@sydsis.co>
2021-02-05 22:22:39 +0000

    updates eqArrays to work recursively

--- eqArrays.js
+++ eqArrays.js
@@ -9,7 +9,11 @@ const assertEqual = function(actual, expected) {
 const eqArrays = function(array1, array2) {
   if (array1.length === array2.length) {
     for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
+      if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+        if (!eqArrays(array1[i], array2[i])) {
+          return false;
+        }
+      } else if (array1[i] !== array2[i]) {
         return false;
       }
     }
@@ -30,3 +34,20 @@ assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+// testing recursive solution
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+
+const arr1 = [[[[[[[[1]]]]]]]];
+const arr2 = [[[[[[[[1]]]]]]]];
+const arr3 = [[[[[[[[2]]]]]]]];
+const arr4 = [[[[[[[[2]]]]]]],2];
+
+assertEqual(eqArrays(arr1, arr2), true);
+assertEqual(eqArrays(arr1, arr3), false);
+assertEqual(eqArrays(arr1, arr3), false);
+assertEqual(eqArrays(arr4, arr3), false);
+assertEqual(eqArrays(arr4, arr4), true);
