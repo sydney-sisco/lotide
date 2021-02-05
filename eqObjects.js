625c59d0cc40829596b6b5632a34f94178ffc1c9
Syd Sisco <hello@sydsis.co>
2021-02-05 21:49:06 +0000

    updates eqObjects function to work with nested objects (recursively)

--- eqObjects.js
+++ eqObjects.js
@@ -33,6 +33,8 @@ const eqObjects = function(object1, object2) {
     for (const key of keys1) {
       if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
         if (!eqArrays(object1[key], object2[key])) return false;
+      } else if (typeof object1[key] === 'object' && typeof object2[key] === 'object') {
+        if (!eqObjects(object1[key], object2[key])) return false;
       } else {
         if (object1[key] !== object2[key]) return false;
       }
@@ -61,3 +63,9 @@ assertEqual(eqObjects(cd, dc), true);
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false);
+
+// testing recursive solution
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
