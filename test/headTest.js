d5361e3d84cd28c565046d02095d46add12bcb81
Syd Sisco <hello@sydsis.co>
2021-02-08 20:34:12 +0000

    adds Mocha and Chai to project. Updates tests for head to use Mocha and Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,24 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual(head([-1,0,1]), -1);
-assertEqual(head(["monday", "tuesday", "wednesday"]), "monday");
-assertEqual(head(['one']), 'one');
-assertEqual(head([], undefined));
+describe('#head', () => {
+  it('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it('returns "5" for ["5"]', () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it('returns -1 for [-1,0,1]', () => {
+    assert.strictEqual(head([-1,0,1]), -1);
+  });
+
+  it('returns monday for ["monday", "tuesday", "wednesday"]', () => {
+    assert.strictEqual(head(['monday', 'tuesday', 'wednesday']), 'monday');
+  });
+
+  it('returns undefined for []', () => {
+    assert.strictEqual(head([], undefined));
+  });
+});
