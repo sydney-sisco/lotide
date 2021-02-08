410fffe2bc9bf0ee144a8a405e63652bf97e42b9
Syd Sisco <hello@sydsis.co>
2021-02-08 20:15:43 +0000

    refactors the middle function to have its tests in a separate file

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-  } else {
-    return false;
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual.toString()} === ${expected.toString()}`);
-  } else {
-    console.log(`🚨🚨🚨 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const middle = function(array) {
   let middle = [];
 
@@ -39,11 +18,4 @@ const middle = function(array) {
   return middle;
 };
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2,3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
