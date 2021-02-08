d2dfe8c34dc9944a13c410868d8de36425a835fa
Syd Sisco <hello@sydsis.co>
2021-02-08 20:02:03 +0000

    removes unneeded require

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,6 @@
 const assertEqual = require('../assertEqual');
 const head = require('../head');
 
-
 assertEqual(head([-1,0,1]), -1);
 assertEqual(head(["monday", "tuesday", "wednesday"]), "monday");
 assertEqual(head(['one']), 'one');
